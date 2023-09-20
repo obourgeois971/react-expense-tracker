9699f7757df654c0214b75aaba188f66eabda80b
Olivier <bourgeoisolivier@hotmail.fr>
2023-09-20 11:20:42 +0200

    Init project

---
+++ src/components/containers/ExpenseInput/ExpenseInput.jsx
@@ -0,0 +1,46 @@
+import { addExpense } from "store/expense/expense-slice";
+import s from "./style.module.css";
+import { useDispatch } from "react-redux";
+
+export function ExpenseInput(props) {
+  // Pour écrire dans le store
+  const dispatch = useDispatch();
+
+  function submit(e) {
+    e.preventDefault();
+    const formDate = new FormData(e.currentTarget);
+    const name = formDate.get("name");
+    const price = formDate.get("price");
+    // console.log(name, price);
+    dispatch(addExpense({ name, price }));
+  }
+  return (
+    <form onSubmit={submit}>
+      <div className="row justify-content-center">
+        <div className="col-12 col-sm-5 col-md-4 col-lg-4 mb-2">
+          <input
+            type="text"
+            className="form-control"
+            placeholder='Ex : "Apple"'
+            name="name"
+          />
+        </div>
+        <div className="col-12 col-sm-2 col-md-4 col-lg-4 mb-2">
+          <input
+            type="number"
+            step="0.01"
+            className="form-control"
+            placeholder="Ex: 3.99"
+            name="price"
+          />
+        </div>
+
+        <div className="col-12 col-sm-2 col-md-4 col-lg-4 mb-2">
+          <button type="submit" className={`btn btn-primary ${s.btn}`}>
+            Add
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
